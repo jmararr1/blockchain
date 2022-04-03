442295139696678fb4d7adfe73847994e31bea24
jmararr1 <marco.arraez.jorge@gmail.com>
2022-04-03 14:24:05 +0200

    creating endpoints

--- dev/api.js
+++ dev/api.js
@@ -1,23 +1,56 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+const uuid = require('uuid/v1');
+
 const app = express();
+const bitcoin = new Blockchain();
+const nodeAdress = uuid().split('-').join('');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/blockchain', function (req, res){
-
+    res.send(bitcoin);
     }
 );
 
 app.get('/transaction', function (req, res){
+    res.send('It works with get!');
+    }
+);
 
+app.post('/transaction', function (req, res){
+    console.log(req.body);
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient)
+    res.json({note: `Transaction will be added in block ${blockIndex}.`});
     }
 );
 
 app.get('/mine', function (req, res){
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index']+1
+    }
 
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+    
+    res.json(
+        {
+        note: "New block mined succesfully",
+        block: newBlock
+        }
+    );
+    
+    bitcoin.createNewTransaction(6.125, "00", nodeAdress);
     }
 );
 
 app.listen(3000, function (){
     console.log('listening on port 3000...');
-
     }
 );
--- dev/blockchain.js
+++ dev/blockchain.js
@@ -49,7 +49,6 @@ function Blockchain () {
         while (hash.substring(0, 4) !== '0000') { 
             nonce++;
             hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-            console.log(hash);
         }
         return nonce;   
     }
